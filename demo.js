732d189b9b871fa2e1ea3e1e2e5ff70a879b8385
Ramaswami.Shakuntala <46563610+RamaswamiM193@users.noreply.github.com>
2021-05-13 01:12:56 +0530

    firstdemo

--- demo.js
+++ demo.js
@@ -1,3 +1,23 @@
 console.log("10"==10);
 console.log("10"===10)
 // this is just a demo
+var arr = ["Pandu","moothi", "Palguthadi"];
+arr.forEach((element,index)=>{
+    console.log(element,index);
+})
+
+//itreating json objects : 
+var obj = {
+    sub_one : "Ramu",
+    sub_two : "Pandu",
+    sub_three : "Balu"
+}
+// obj.forEach((element,index)=>{
+// console.log(element,index)
+// })
+for(var key in obj){
+    console.log(key, obj[key])
+}
+
+var sys = Symbol(100);
+console.log(sys)
